6362eb7837b9d2c06c04fd857ce65867746c75f2
gxuk1000 <gxuk1000@naver.com>
2016-01-25 08:22:27 +0900

    test

--- Server/src/main/java/com/secsm/main/ProjectController.java
+++ Server/src/main/java/com/secsm/main/ProjectController.java
@@ -12,10 +12,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class ProjectController {
 	private static final Logger logger = LoggerFactory.getLogger(ProjectController.class);
 	
+	/** 교육 Main */
 	@RequestMapping(value = "/project", method = RequestMethod.GET)
 	public String MainController_index(HttpServletRequest request) {
 		logger.info("project Page");
 
 		return "project";
+	}
+	
+	/** 프로젝트 등록 */
+	@RequestMapping(value = "/project/add", method = RequestMethod.GET)
+	public String MainController_newProject(HttpServletRequest request) {
+		logger.info("project add Page");
+
+		return "ProjectAdd";
+	}
+	
+	/** 프로젝트 수정 */
+	@RequestMapping(value = "/project/update", method = RequestMethod.GET)
+	public String MainController_updateProject(HttpServletRequest request) {
+		logger.info("project update Page");
+
+		return "ProjectUpdate";
 	}
 }
